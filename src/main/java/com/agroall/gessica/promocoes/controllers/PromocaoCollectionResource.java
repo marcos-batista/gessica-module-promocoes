a4e7d0ba0e51791621b53de3db61101d1f25f4b9
Marcos Batista <mab.marcos@gmail.com>
2018-06-24 22:34:38 -0300

    Atualizando dataObjects

--- src/main/java/com/agroall/gessica/promocoes/controllers/PromocaoCollectionResource.java
+++ src/main/java/com/agroall/gessica/promocoes/controllers/PromocaoCollectionResource.java
@@ -3,6 +3,7 @@ package com.agroall.gessica.promocoes.controllers;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.agroall.gessica.promocoes.services.PromocaoService;
 import com.agroall.gessica.services.Service;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/promocoes")
 public class PromocaoCollectionResource extends DataCollectionResourceControllerImpl<Promocao> {
 	
